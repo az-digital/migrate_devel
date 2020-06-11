487079caf164a7a9c708c50c48304ac34c86b12b
Andrew Macpherson <hopscotch23@gmail.com>
2020-06-11 21:30:52 +0100

    Issue #2938677: Register MigrateDevelCommands services class for Drush 9+

---
+++ src/Commands/MigrateDevelCommands.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\migrate_devel\Commands;
+
+use Drush\Commands\DrushCommands;
+
+/**
+ * Extends migrate commands with debugging options.
+ */
+class MigrateDevelCommands extends DrushCommands {
+
+}
